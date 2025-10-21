5592fb9697decac99c9eb03ea3b1b2989470bcaa
evasheveleva <144834013+evasheveleva@users.noreply.github.com>
2025-10-21 21:30:17 +0300

    Update states.py

--- states.py
+++ states.py
@@ -8,4 +8,17 @@ class User(StatesGroup):
     activity_lvl = State()
     water_goal = State()
     calorie_goal = State()
+    
+class EditProfileStates(StatesGroup):
+    choosing_field = State()
+    editing_weight = State()
+    editing_height = State()
+    editing_age = State()
+    editing_activity = State()
+    editing_water_goal = State()
+    editing_calorie_goal = State()
+
+class DeleteProfileStates(StatesGroup):
+    confirmation = State()
+
 
