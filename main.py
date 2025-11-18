a283e81cf36c92d378b1bc7c583ee3a319b68267
olyashabanovaa <144255262+olyashabanovaa@users.noreply.github.com>
2025-11-18 22:25:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import asyncio
+import sys
 from aiogram import Bot, Dispatcher
 from config import TELEGRAM_TOKEN
 from middlewares import LoggingMiddleware
 from handlers import setup_handlers
-
-bot = Bot(token=TELEGRAM_TOKEN)
-dp = Dispatcher()
-
-dp.message.middleware(LoggingMiddleware())
-setup_handlers(dp)
+from db import init_db
 
 async def main():
+    init_db()
+    
+    bot = Bot(token=TELEGRAM_TOKEN)
+    dp = Dispatcher()
+    
+    dp.message.middleware(LoggingMiddleware())
+    setup_handlers(dp)
+    
     print("Бот запущен!")
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
         print('Бот выключен')
+    except Exception as e:
+        print(f'Ошибка при запуске бота: {e}')
+        import traceback
+        traceback.print_exc()
+        sys.exit(1)
