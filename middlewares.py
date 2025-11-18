878207fc8407647a8cd922d35919c71344e275e5
olyashabanovaa <144255262+olyashabanovaa@users.noreply.github.com>
2025-11-18 22:25:58 +0300

    Update middlewares.py

--- middlewares.py
+++ middlewares.py
@@ -1,7 +1,20 @@
 from aiogram import BaseMiddleware
-from aiogram.types import Message
+from aiogram.types import Message, TelegramObject
+from typing import Callable, Dict, Any, Awaitable
+
 
 class LoggingMiddleware(BaseMiddleware):
-    async def __call__(self, handler, event: Message, data: dict):
-        print(f"Получено сообщение: {event.text}")
+    
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any]
+    ) -> Any:
+        if isinstance(event, Message):
+            user_id = event.from_user.id if event.from_user else None
+            username = event.from_user.username if event.from_user else None
+            text = event.text or event.caption or "[медиа]"
+            print(f"[{user_id} @{username}]: {text}")
+        
         return await handler(event, data)
