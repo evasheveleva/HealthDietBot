a92f4dde1908ebc36522f45eddb44fc6cfcda8b9
olyashabanovaa <144255262+olyashabanovaa@users.noreply.github.com>
2025-11-18 22:26:21 +0300

    Update states.py

--- states.py
+++ states.py
@@ -2,23 +2,29 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class User(StatesGroup):
+    """Состояния для регистрации пользователя"""
+    gender = State()
     weight = State()
     height = State()
     age = State()
-    activity_lvl = State()
-    water_goal = State()
-    calorie_goal = State()
-    
+    activity_level = State()
+
+
 class EditProfileStates(StatesGroup):
+    """Состояния для редактирования профиля"""
     choosing_field = State()
+    editing_gender = State()
     editing_weight = State()
     editing_height = State()
     editing_age = State()
     editing_activity = State()
-    editing_water_goal = State()
-    editing_calorie_goal = State()
+
+
+class PhotoRecognitionStates(StatesGroup):
+    """Состояния для обработки фото блюда"""
+    waiting_for_correction = State()
+
 
 class DeleteProfileStates(StatesGroup):
+    """Состояния для удаления профиля"""
     confirmation = State()
-
-
