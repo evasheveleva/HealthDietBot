6a8339e00813c3fa4109fc528b09dd28ecb4ff65
olyashabanovaa <144255262+olyashabanovaa@users.noreply.github.com>
2025-12-20 21:11:46 +0300

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -1,12 +1,21 @@
 def calc_calorie(weight: float, height: float, age: float, gender: str, activity_level: float) -> float:
     if gender.lower() == 'male':
         bmr = 10 * weight + 6.25 * height - 5 * age + 5
-    else:  # female
+    else:
         bmr = 10 * weight + 6.25 * height - 5 * age - 161
 
     tdee = bmr * activity_level
     
     return round(tdee)
+
+
+def calc_calorie_with_goal(base_calories: float, goal_type: str) -> float:
+    if goal_type == 'lose':
+        return round(base_calories * 0.9)
+    elif goal_type == 'gain':
+        return round(base_calories * 1.1)
+    else:
+        return round(base_calories)
 
 
 def calc_water(weight: float, activity_level: float) -> float:
@@ -17,4 +26,5 @@ def calc_water(weight: float, activity_level: float) -> float:
     total_water = base_water + activity_bonus
     
     return round(total_water)
+
 
